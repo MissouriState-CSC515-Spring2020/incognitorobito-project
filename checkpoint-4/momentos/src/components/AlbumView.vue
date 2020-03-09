76b7d0fbf2817d5df1d0a8fafddc45af79840186
Mark Raymond Jr <markrjr@live.com>
2020-03-08 21:29:20 -0500

    Add PWA support.

---
+++ checkpoint-4/momentos/src/components/AlbumView.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <div>
+    <div class="flexbox col">
+      <h1 class="centered">Hawaii</h1>
+      <p class="centered">63 photos</p>
+    </div>
+
+    <div class="columns p-2">
+      <div class="column">
+        <h5 class="mb-2"><router-link to="/photo">Last Saturday</router-link></h5>
+        <div class="flexbox row photo-masonry">
+          <router-link class="masonry-brick" to="/photo"></router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AlbumView extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
